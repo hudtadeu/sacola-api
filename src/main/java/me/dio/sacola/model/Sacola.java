c8f0af79f4d4727b199d0dabb4cb86c034733609
Hudson Tadeu <hudinvesting@gmail.com>
2023-08-15 15:44:25 -0300

    Configurações classe sacola

--- src/main/java/me/dio/sacola/model/Sacola.java
+++ src/main/java/me/dio/sacola/model/Sacola.java
@@ -1,5 +1,32 @@
 package me.dio.sacola.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import me.dio.sacola.enumeration.FormaPagamento;
+
+@Entity
 public class Sacola {
-  
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  @ManyToOne(fetch = FetchType.LAZY, optional = false)
+  @JsonIgnore
+  private Cliente cliente;
+  @OneToMany(cascade = CascadeType.ALL)
+  private List<Item> itens;
+  private Double valorTotal;
+
+  @Enumerated
+  private FormaPagamento Formapagamento;
+  private boolean fechada;
 }
