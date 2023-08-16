6b4891bb58f034b3756a28f67f60a587cce0a7bd
Hudson Tadeu <hudinvesting@gmail.com>
2023-08-16 16:44:05 -0300

    Construção classe Produto

--- src/main/java/me/dio/sacola/model/Produto.java
+++ src/main/java/me/dio/sacola/model/Produto.java
@@ -1,5 +1,34 @@
 package me.dio.sacola.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@NoArgsConstructor
 public class Produto {
-  
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  private String nome;
+  private Double valorUnitario;
+  @Builder.Default
+  private Boolean disonivel = true;
+  @ManyToOne
+  @JsonIgnore
+  private Restaurante restaurante;
 }
