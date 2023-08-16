91c595205ef0a786525a9c333f33bec48ac7757e
Hudson Tadeu <hudinvesting@gmail.com>
2023-08-16 14:59:36 -0300

    Melhorando o código

--- src/main/java/me/dio/sacola/model/Sacola.java
+++ src/main/java/me/dio/sacola/model/Sacola.java
@@ -3,6 +3,8 @@ package me.dio.sacola.model;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Enumerated;
@@ -12,9 +14,18 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import lombok.Builder;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import me.dio.sacola.enumeration.FormaPagamento;
 
+@AllArgsConstructor
+@Builder
+@Data
 @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@NoArgsConstructor
 public class Sacola {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
@@ -29,61 +40,4 @@ public class Sacola {
   @Enumerated
   private FormaPagamento FormaPagamento;
   private boolean fechada;
-
-  public Sacola(Long id, Cliente cliente, List<Item> itens, Double valorTotal, FormaPagamento formaPagamento, Boolean fechada) {
-    this.id = id;
-    this.cliente =cliente;
-    this.itens = itens;
-    this.valorTotal = valorTotal;
-    FormaPagamento = formaPagamento;
-    this.fechada = fechada;
-  }
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public Cliente getCliente() {
-    return cliente;
-  }
-
-  public void setCliente(Cliente cliente) {
-    this.cliente = cliente;
-  }
-
-  public List<Item> getItens() {
-    return itens;
-  }
-
-  public void setItens(List<Item> itens) {
-    this.itens = itens;
-  }
-
-  public Double getValorTotal() {
-    return valorTotal;
-  }
-
-  public void setValorTotal(Double valorTotal) {
-    this.valorTotal = valorTotal;
-  }
-
-  public FormaPagamento getFormaPagamento() {
-    return FormaPagamento;
-  }
-
-  public void setFormaPagamento(FormaPagamento formaPagamento) {
-    FormaPagamento = formaPagamento;
-  }
-
-  public boolean isFechada() {
-    return fechada;
-  }
-
-  public void setFechada(boolean fechada) {
-    this.fechada = fechada;
-  }
 }
