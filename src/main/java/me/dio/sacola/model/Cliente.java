0605e4288050612b9773a4f9a6f8621aa12beb31
Hudson Tadeu <hudinvesting@gmail.com>
2023-08-16 16:14:57 -0300

    Construção classe Cliente

--- src/main/java/me/dio/sacola/model/Cliente.java
+++ src/main/java/me/dio/sacola/model/Cliente.java
@@ -1,5 +1,30 @@
 package me.dio.sacola.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@NoArgsConstructor
 public class Cliente {
-  
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  private String nome;
+
+  @Embedded
+  private Endereco endereco;
 }
