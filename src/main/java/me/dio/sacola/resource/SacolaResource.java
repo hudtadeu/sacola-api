ec5a171a6e19cb674648b2a86ff149281aa557f2
Hudson Tadeu <hudinvesting@gmail.com>
2023-08-29 09:59:23 -0300

    Pacote dto

---
+++ src/main/java/me/dio/sacola/resource/SacolaResource.java
@@ -0,0 +1,13 @@
+package me.dio.sacola.resource;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/ifood-devweek/sacolas")
+public class SacolaResource {
+  
+  public Item incluirItemNaSacola() {
+    
+  }
+}
