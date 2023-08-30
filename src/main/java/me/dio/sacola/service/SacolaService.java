738ee24c204b35e3cccd0622b22a798e69cf474d
Hudson Tadeu <hudinvesting@gmail.com>
2023-08-30 15:26:09 -0300

    Método de fechar sacola finalizado

--- src/main/java/me/dio/sacola/service/SacolaService.java
+++ src/main/java/me/dio/sacola/service/SacolaService.java
@@ -5,7 +5,7 @@ import me.dio.sacola.model.Sacola;
 import me.dio.sacola.resource.dto.ItemDto;
 
 public interface SacolaService {
-  Item icluirItemNaSacola(ItemDto itemDto);
+  Item incluirItemNaSacola(ItemDto itemDto);
   Sacola verSacola(Long id);
   Sacola fecharSacola(Long id, int formaPagamento);
 }
